09e84f83838927587a79050faf3dd1d0ee7473d9
jiulongteng <jiulongteng@myweimai.com>
2018-06-01 17:01:54 +0800

    添加各种api

--- lintrules/src/main/groovy/com/ytjojo/lintjar/plugin/LintRulesPlugin.groovy
+++ lintrules/src/main/groovy/com/ytjojo/lintjar/plugin/LintRulesPlugin.groovy
@@ -1,29 +1,52 @@
 package com.ytjojo.lintjar.plugin
 
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.BaseExtension
+import com.android.build.gradle.BasePlugin
+import com.android.build.gradle.LibraryExtension
+import com.android.build.gradle.TestedExtension
+import com.android.build.gradle.api.AndroidSourceSet;
+import com.android.build.gradle.api.BaseVariant
+import com.android.build.gradle.api.TestVariant
+import com.android.build.gradle.api.UnitTestVariant
 import com.android.build.gradle.internal.dsl.LintOptions
 import com.android.build.gradle.tasks.Lint
 import com.android.builder.model.AndroidProject
+import com.android.builder.model.SourceProvider
 import org.apache.commons.io.FileUtils
 import org.apache.commons.io.IOUtils
+import org.gradle.api.Action
+import org.gradle.api.DomainObjectSet
 import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.Dependency
 import org.gradle.api.artifacts.DependencySet
 import org.gradle.api.artifacts.ProjectDependency
 import org.gradle.api.file.FileCollection
+import org.gradle.api.file.FileTree
 import org.gradle.api.internal.artifacts.repositories.DefaultMavenLocalArtifactRepository
+import org.gradle.api.internal.file.FileTreeInternal
+import org.gradle.api.internal.file.UnionFileTree
+import org.gradle.api.internal.jvm.ClassDirectoryBinaryNamingScheme
+import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.TaskState
+import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.internal.impldep.org.apache.maven.artifact.repository.MavenArtifactRepository
+import org.gradle.jvm.tasks.Jar
 import org.gradle.tooling.GradleConnector
 
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.stream.Stream
 import java.util.zip.ZipFile
 
 class LintRulesPlugin implements Plugin<Project> {
-
+    Project mProject;
     void apply(Project project) {
+        this.mProject = project;
         def lintJarName
         def lintDir
 
@@ -140,7 +163,8 @@ class LintRulesPlugin implements Plugin<Project> {
 
 
         String maventLocal = java.lang.System.getProperty("user.home") + "/.m2/repository"
-        String maventLocal2 = project.repositories.mavenLocal().url.toString() - 'file:' + group.replace('.', '/') + '/' + name.replace('.', '/') + '/' + version // name  对应的就是依赖的 artifactId
+        String maventLocal2 = project.repositories.mavenLocal().url.toString() - 'file:' + group.replace('.', '/') + '/' + name.replace('.', '/') + '/' + version
+        // name  对应的就是依赖的 artifactId
         // name  对应的就是依赖的 artifactId
         def defaultGradleUserHome = new File(java.lang.System.getProperty("user.home") + "/.gradle")
         def gradleUserHome = System.getProperty('gradle.user.home')
@@ -182,7 +206,7 @@ class LintRulesPlugin implements Plugin<Project> {
 
     }
 
-    private void exlude(Project project,String group,List<String> modules) {
+    private void exlude(Project project, String group, List<String> modules) {
         project.configurations {
             project.getConfigurations().all {
                 for (String item : modules) {
@@ -284,8 +308,8 @@ class LintRulesPlugin implements Plugin<Project> {
                         rootProject.subprojects { subProject ->
                             String lintDir = "${subProject.buildDir.absolutePath}/intermediates/lint/"
                             println(lintDir + "  " + it.absolutePath)
-                            File lintJarFile =  new File(lintDir +"lint.jar")
-                            if(lintJarFile.exists()){
+                            File lintJarFile = new File(lintDir + "lint.jar")
+                            if (lintJarFile.exists()) {
                                 lintJarFile.delete()
                             }
                             FileUtils.copyFile(it, lintJarFile)
@@ -299,17 +323,13 @@ class LintRulesPlugin implements Plugin<Project> {
             }
         }
     }
-    private void download(String repoDepUrl,File depFile) {
+
+    private void download(String repoDepUrl, File depFile) {
         if (!repoDepUrl.startsWith('file:')) {
             project.logger.info 'repoDepUrl : ' + repoDepUrl
             FileUtils.copyURLToFile(new URL(repoDepUrl), depFile)
         }
     }
-
-
-
-
-
 
 
     static void getJarFromAar(Project project, File aar) {
@@ -370,7 +390,8 @@ class LintRulesPlugin implements Plugin<Project> {
         AndroidProject modelProject = gradleConn.connect().getModel(AndroidProject.class)
         return modelProject
     }
-    private HashSet<String> getRepUrls(Project project){
+
+    private HashSet<String> getRepUrls(Project project) {
         def repoUrls = new HashSet()
         repoUrls.add(project.repositories.jcenter().url.toString())
         repoUrls.add(project.repositories.mavenCentral().url.toString())
@@ -387,6 +408,7 @@ class LintRulesPlugin implements Plugin<Project> {
 
 
     }
+
     static String getDependencyByDownload(Project project, String dependency, HashSet<String> repoUrls, String mavenLocalDepPath) {
         def aarPath = ''
         try {
@@ -417,7 +439,7 @@ class LintRulesPlugin implements Plugin<Project> {
         return aarPath
     }
 
-    private void wrong(Project project){
+    private void wrong(Project project) {
 
 
         Configuration configuration = project.getConfigurations().create("providedAar") {
@@ -454,12 +476,12 @@ class LintRulesPlugin implements Plugin<Project> {
         }
     }
 
-    private void changeLog(Project project){
+    private void changeLog(Project project) {
         //redirect warning log to info log
         def listenerBackedLoggerContext = project.getLogger().getMetaClass().getProperty(project.getLogger(), "context")
         def originalOutputEventListener = listenerBackedLoggerContext.getOutputEventListener()
         def originalOutputEventLevel = listenerBackedLoggerContext.getLevel()
-        listenerBackedLoggerContext.setOutputEventListener({def outputEvent ->
+        listenerBackedLoggerContext.setOutputEventListener({ def outputEvent ->
             def logLevel = originalOutputEventLevel.name()
             if (!("QUIET".equalsIgnoreCase(logLevel) || "ERROR".equalsIgnoreCase(logLevel))) {
                 if ("WARN".equals(outputEvent.getLogLevel().name())) {
@@ -478,4 +500,278 @@ class LintRulesPlugin implements Plugin<Project> {
         })
     }
 
+    private void configureR2Generation(Project project, DomainObjectSet<? extends BaseVariant> variants) {
+        variants.all { variant ->
+            def outputDir = new File(project.buildDir, "generated/source/r2/${variant.dirName}")
+            def task = project.tasks.create("generate${variant.name.capitalize()}R2")
+            task.outputs.dir(outputDir)
+            variant.registerJavaGeneratingTask(task, outputDir)
+            def rpackage = getPackageName(variant)
+            def once = new AtomicBoolean(false);
+            variant.getOutputs().each { output ->
+                def processResources = output.processResources;
+                task.dependsOn(processResources)
+                if (once.compareAndSet(false, task)) {
+                    def pathToR = rpackage.replace('.', File.separatorChar)
+                    def rFile = new File(new File(processResources.sourceOutputDir, pathToR), "R.java")
+                    task.getInputs().file(rFile)
+                    task.doLast {
+
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    // Parse the variant's main manifest file in order to get the package id which is used to create
+    // R.java in the right place.
+    private String getPackageName(BaseVariant variant) {
+        def slurper = new XmlSlurper(false, false)
+        variant.sourceSets.listIterator().collect { it.manifestFile }
+        def list = variant.sourceSets.map { it.manifestFile }
+
+        // According to the documentation, the earlier files in the list are meant to be overridden by the later ones.
+        // So the first file in the sourceSets list should be main.
+        def result = slurper.parse(list[0])
+        return result.getProperty("@package").toString()
+    }
+
+    public TestedExtension getAndroidExtension(Project project) {
+        return (TestedExtension)project.getProperties().get("android")
+    }
+
+    public void addAppAction(Action<BaseVariant> action, AppExtension extension) {
+        extension.getApplicationVariants().all(action)
+    }
+
+    public void addLibraryAction(Action<BaseVariant> action, LibraryExtension extension) {
+        extension.getLibraryVariants().all(action)
+    }
+
+    public DomainObjectSet<? extends BaseVariant> getAndroidVariants(Project project) {
+        DomainObjectSet<? extends BaseVariant> domainObjectSet = null
+        TestedExtension androidExtension = null;
+        project.getPlugins().withType()
+        project.getPlugins().withType(AppPlugin.class, {
+            androidExtension = project.getExtensions().getByType(AppExtension.class);
+            domainObjectSet = ((AppExtension) androidExtension).getApplicationVariants()
+
+        });
+
+        project.getPlugins().withType(LibraryPlugin.class, { libraryPlugin ->
+            androidExtension = project.getExtensions().getByType(LibraryExtension.class);
+            domainObjectSet = ((LibraryExtension) androidExtension).getLibraryVariants();
+
+        });
+
+//        project.getPlugins().withType(FeaturePlugin.class, {atomPlugin ->
+//            androidExtension = project.getExtensions().getByType(FeatureExtension.class);
+//           domainObjectSet = ((FeatureExtension)androidExtension).getFeatureVariants();
+//        });
+
+        return domainObjectSet
+
+    }
+
+    protected void addAllSourceJarDependence(Project project,DomainObjectSet<? extends BaseVariant> domainObjectSet) {
+
+        domainObjectSet.all{variant ->
+            Jar sourcesJarTask = project.getTasks().create("sources" + capitalize((CharSequence) variant.getName()) + "Jar", Jar.class, {jar ->
+                jar.setDescription("Generate the sources jar for the " + variant.getName() + " variant");
+                jar.setGroup("jar");
+
+                jar.setClassifier("sources");
+                jar.setAppendix(variant.getName());
+                jar.from(getJavaTask(variant).property("source"));
+            });
+
+            allSourcesJarTask.dependsOn(sourcesJarTask);
+            if (publishVariant(variant)) {
+                getProject().getArtifacts().add(Dependency.ARCHIVES_CONFIGURATION, sourcesJarTask);
+            }
+        }
+    }
+    protected boolean publishVariant(BaseVariant variant) {
+        if (variant instanceof TestVariant || variant instanceof UnitTestVariant) {
+            return false;
+        }
+
+        return  getAndroidExtension(mProject).getDefaultPublishConfig().equals(variant.getName());
+    }
+
+    Task allSourcesJarTask
+    private void createAllSourcesJarTask(){
+        allSourcesJarTask = project.getTasks().create("sourcesJar",  {asjTask ->
+            asjTask.setDescription("Generate the sources jar for all variants");
+            asjTask.setGroup("jar");
+        });
+    }
+
+    public void applyPlugins(Class<Plugin<Project>> pluginClass){
+        mProject.getPluginManager().apply(pluginClass)
+    }
+
+    public FileCollection getCompileClasspath(BaseVariant variant) {
+        return getJavaTask(variant).getClasspath();
+    }
+    /**
+     * Returns the classpath used to compile this source.
+     *
+     * @return The classpath. Never returns null.
+     * @see SourceSet#getCompileClasspath()
+     */
+    protected FileCollection getCompileClasspath(AndroidSourceSet androidSourceSet) {
+        return getProject().getConfigurations().getByName(androidSourceSet.getCompileConfigurationName());
+    }
+
+    /**
+     * All Java source files for this source set. This includes, for example, source which is directly compiled, and
+     * source which is indirectly compiled through joint compilation.
+     *
+     * @return the Java source. Never returns null.
+     * @see SourceSet#getAllJava()
+     */
+    protected static FileTree getAllJava(AndroidSourceSet androidSourceSet) {
+        return androidSourceSet.getJava().getSourceFiles();
+    }
+
+
+    /**
+     * Returns the name of a task for this source set.
+     *
+     * @param verb The action, may be null.
+     * @param target The target, may be null
+     * @return The task name, generally of the form ${verb}${name}${noun}
+     * @see SourceSet#getTaskName(String, String)
+     */
+    protected static String getTaskName(AndroidSourceSet sourceSet, String verb, String target) {
+        return new ClassDirectoryBinaryNamingScheme(sourceSet.getName()).getTaskName(verb, target);
+    }
+
+
+    public static FileTree getAllJava(BaseVariant variant) {
+        return getJavaTask(variant).getSource();
+    }
+
+    /**
+     * @see SourceSet#getOutput()
+     */
+    public FileTree getOutput(BaseVariant variant) {
+        return getProject().fileTree(getJavaTask(variant).getDestinationDir());
+    }
+
+    public Project getProject(){
+        return  mProject
+    }
+
+    /**
+     * @see SourceSet#getOutput()
+     */
+    protected FileCollection getOutput(AndroidSourceSet androidSourceSet) {
+        Stream<BaseVariant> testVariants = Stream.concat(getTestVariants().stream(), getUnitTestVariants().stream());
+        Stream<BaseVariant> variants = Stream.concat(getAndroidVariants().stream(), testVariants);
+        variants.
+        FileTreeInternal[] sourceTrees = variants
+                .filter{variant -> getJavaTask(variant) != null}
+                .filter{variant -> variant.getSourceSets().contains(androidSourceSet)}
+                .map{variant -> getJavaTask(variant).getDestinationDir()}
+                .map{outputDir -> (FileTreeInternal) getProject().fileTree(outputDir)}
+                .toArray(new FileTreeInternal[0]);
+        return new UnionFileTree(sourceTrees);
+    }
+
+    private Map<String, String> excludeProperties(Configuration configuration,String group, String module) {
+        configuration.exclude( ImmutableMap.<String, String>builder()
+                .put("group", group)
+                .put("module", module)
+                .build())
+
+    }
+
+    protected void withBasePlugin(Action<Plugin> action) {
+        project.getPlugins().withType(getBasePlugin(), action);
+    }
+
+    protected JavaPluginConvention getJavaPluginConvention() {
+        return project.getConvention().getPlugin(JavaPluginConvention.class);
+    }
+
+    protected Class<? extends Plugin> getBasePlugin() {
+        return JavaBasePlugin.class;
+    }
+
+    public FileCollection getCurentClassPath(BaseVariant variant,TestedExtension extension){
+        return  getJavaTask(variant).classpath + project.files(extension.androidBuilder.getBootClasspath(false))
+    }
+
+    /**
+     * Returns directory for plugin's private working directory for argument
+     * workDir = new File(project.buildDir, "android-scala")
+     * @param variant the Variant
+     * @return
+     */
+    public File getVariantWorkDir(File workDir,BaseVariant variant) {
+       return new File([workDir, "variant", variant.name].join(File.separator))
+    }
+
+    public BaseExtension getBaseExtension(){
+        def androidPlugin = ['android', 'com.android.application', 'android-library', 'com.android.library',
+                             'com.android.test', 'com.android.feature']
+                .collect { project.plugins.findPlugin(it) as BasePlugin }
+                .find { it != null }
+
+        if (androidPlugin == null) {
+            throw new GradleException('You must apply the Android plugin or the Android library plugin before using the groovy-android plugin')
+        }
+        def androidExtension = project.extensions.getByName("android") as BaseExtension
+        return androidExtension
+
+    }
+
+    public static getRuntimeJars(BasePlugin plugin, BaseExtension extension) {
+        if (plugin.metaClass.getMetaMethod('getRuntimeJarList')) {
+            return plugin.runtimeJarList
+        }
+
+        if (extension.metaClass.getMetaMethod('getBootClasspath')) {
+            return extension.bootClasspath
+        }
+
+        return plugin.bootClasspath
+    }
+
+    public static List<String> getJavaTaskCompilerArgs(JavaCompile javaTask, boolean skipJavaC) {
+        def compilerArgs = javaTask.options.compilerArgs
+
+        if (skipJavaC) {
+            // if we skip java c the java compiler will still look for the annotation processor directory
+            // we should create it for it.
+            compilerArgs.findAll { !it.startsWith('-') }
+                    .each { new File(it).mkdirs() }
+        }
+
+        return compilerArgs
+    }
+
+
+    public static String getVariantName(BaseVariant variant) {
+        return variant.name
+    }
+
+    public static JavaCompile getJavaTask(BaseVariant variant) {
+        // just get actual javac we don't support jack.
+
+        return  variant.hasProperty('javaCompiler') ? variant.javaCompiler : variant.javaCompile
+    }
+
+    public static Iterable<SourceProvider> getSourceProviders(BaseVariant variantData) {
+        return variantData.sourceSets
+    }
+
+
+//    private static List<File> getGeneratedSourceDirs(BaseVariant variantData) {
+//        return variantData.getSourceFolders(SourceKind.JAVA).collect { it.dir }
+//    }
 }
